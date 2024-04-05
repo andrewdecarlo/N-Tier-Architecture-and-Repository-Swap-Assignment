8c0023a1674b271f3c37bf3b5dd79fe94aa7022b
andrewdecarlo <34290135+andrewdecarlo@users.noreply.github.com>
2024-04-05 12:43:27 -0400

    Add project files.

---
+++ RazorCRUDUI/Pages/Items/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorCRUDUI.Data;
+using RazorCRUDUI.Models;
+using RazorRepoUI.Data;
+
+namespace RazorCRUDUI.Pages.Items
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IItemRepository _repo;
+
+        public CreateModel(IItemRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public ItemModel ItemModel { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _repo.InsertItemAsync(ItemModel);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
